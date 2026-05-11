322a01c8b624733f62b99143767513d173461d2d
SRP0674X <stevenrojaspalacios@gmail.com>
2026-05-10 20:41:47 -0600

    Update ReservaBuseta.cpp

--- ReservaBuseta.cpp
+++ ReservaBuseta.cpp
@@ -3,6 +3,9 @@
 ReservaBuseta::ReservaBuseta() {
 	
 }
+ReservaBuseta::ReservaBuseta(string nombreTurista, string numeroPasaporte, string aeropuertoLlegada, string hotelDestino, string fechaReserva, string horaReserva, string cantidadPasajeros, string distanciaKm, string costoPorKm, string costoPorPasajero, string prioridad, string placaBuseta, string nombreChofer, string costoTotal) {
+		
+	}
 
 ReservaBuseta::~ReservaBuseta() {
 	
