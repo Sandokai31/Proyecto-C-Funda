bc33e45433b3dc6cc49bbadfc56c7e996269f4d5
SRP0674X <stevenrojaspalacios@gmail.com>
2026-05-18 18:40:41 -0600

    Update ReservaBuseta.h

--- ReservaBuseta.h
+++ ReservaBuseta.h
@@ -7,14 +7,26 @@ using namespace std;
 class ReservaBuseta {
 public:
 	ReservaBuseta();
-	ReservaBuseta(string nombreTurista, string numeroPasaporte, string aeropuertoLlegada, string hotelDestino, string fechaReserva, string horaReserva, string cantidadPasajeros, string distanciaKm, string costoPorKm, string costoPorPasajero, string prioridad, string placaBuseta, string nombreChofer, string costoTotal);
+	ReservaBuseta(string nombreTurista, string numeroPasaporte, string aeropuertoLlegada, string hotelDestino, string fechaReserva, 
+				  string horaReserva, int cantidadPasajeros, int distanciaKm, int costoPorKm, int costoPorPasajero, 
+				  int prioridad, string placaBuseta, string nombreChofer, int costoTotal);
+	
+	
 	~ReservaBuseta();
+	
+	
 	void setnombreTurista(string nombreTurista);
 	void setnumeroPasaporte(string numeroPasaporte);
 	void setaeropuertoLlegada(string aeropuertoLlegada);
+	void sethotelDestino(string hotelDestino);
+	void setfechaReserva(string fechaReserva);
+	void sethoraReserva (string horaReserva);
+	
+	void setcantidadPasajeros (int cantidadPasajeros);
 	void setdistanciaKm(int distanciaKm);
 	void setcostoPorKm(int costoPorKm);
 	void setcostoPorPasajero(int costoPorPasajero);
+	
 	void setprioridad(int prioridad);
 	void setplacaBuseta(string placaBuseta);
 	void setnombreChofer(string nombreChofer);
@@ -23,15 +35,24 @@ public:
 	string getnombreTurista();
 	string getnumeroPasaporte();
 	string getaeropuertoLlegada();
+	string gethotelDestino ();
+	string getfechaReserva ();
+	string gethoraReserva ();
+	
+	
+	int getcantidadPasajeros ();
 	int getdistanciaKm();
 	int getcostoPorKm();
 	int getcostoPorPasajero();
+	
+	
 	int getprioridad();
 	string getplacaBuseta();
 	string getnombreChofer();
 	int getcostoTotal();
-
+	
 	string toString();
+	
 private:
 	string nombreTurista;
 	string numeroPasaporte;
@@ -50,4 +71,3 @@ private:
 };
 
 #endif
-
