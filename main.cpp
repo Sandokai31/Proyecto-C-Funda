df7479cb64033c5611c26263db771e3d126ac248
Sandokai31 <matrova31@gmail.com>
2026-05-22 18:47:17 -0600

    Refactor main function to include reservation details
    Se agrego una base para agendar citas, quedo faltando el nombre del chofer y la placa, tambien el nivel de importancia

--- main.cpp
+++ main.cpp
@@ -4,13 +4,29 @@ using namespace std;
 
 int main () {
 	
+	string nombreTurista;
+	string numeroPasaporte;
+	string aeropuertoLlegada;
+	string hotelDestino;
+	string fechaReserva;
+	string horaReserva;
+	int cantidadPasajeros;
+	int distanciaKm;
+	int costoPorKm=500;
+	int costoPorPasajero=20000;
+	int costoTotal=0;
+	
+	
 	int op=0;
+	
+	int reserva=0; //reserva no es igual a reservas
 	ReservaBuseta reservas[10];
+	
 	
 	cout <<"-----Reserva de Busetas KMS-----"<<endl;
 	cout << "--Bienvenido al sistema de reservas KMS" << endl;
 	do {
-		cout << endl << "--Elija la opcion que desea realizar. Para ello, digite el numero que aparece a la izquierda de cada opcion" << endl;
+		cout << endl << "--Elija la opcion que desea realizar--" << endl;
 		cout << endl << "0- Registrar 5 reservas (valores por defecto)" << endl;
 		cout << "1. Registrar reserva" << endl;
 		cout << "2. Mostrar todas las reservas" << endl;
@@ -20,30 +36,89 @@ int main () {
 		cout << "6. Salir"<< endl;
 		cout << endl << "Opcion: ";
 		cin >> op;
+		
 		switch (op) {
 			case 1: {
-				break;
-			}
+				if(reserva<10){
+				
+				cout<<"Diguite su nombre "<<endl;
+				cin>>nombreTurista;
+				reservas[reserva].setnombreTurista(nombreTurista);
+				
+				cout<<"Diguite su Numero de Pasaporte "<<endl;
+				cin>>numeroPasaporte;
+				reservas[reserva].setnumeroPasaporte(numeroPasaporte);
+				
+				cout<<"Diguite Aeropuerto de llegada "<<endl;
+				cin>>aeropuertoLlegada;
+				reservas[reserva].setaeropuertoLlegada(aeropuertoLlegada);
+				
+				cout<<"Diguite su Hotel destino "<<endl;
+				cin>>hotelDestino;
+				reservas[reserva].sethotelDestino(hotelDestino);
+				
+				cout<<"Fecha de reserva (dd/mm/aaaa) "<<endl;
+				cin>>fechaReserva;
+				reservas[reserva].setfechaReserva(fechaReserva);
+				
+				cout<<"Hora de reserva (h:m) "<<endl;
+				cin>>horaReserva;
+				reservas[reserva].sethoraReserva(horaReserva);
+				
+				cout<<"Cantidad de pasajeros "<<endl;
+				cin>>cantidadPasajeros;
+				reservas[reserva].setcantidadPasajeros(cantidadPasajeros);
+				
+				reservas[reserva].setcostoPorPasajero(costoPorPasajero);
+				
+				cout<<"Distancia en Km "<<endl;
+				cin>>distanciaKm;
+				reservas[reserva].setdistanciaKm(distanciaKm);
+				
+				reservas[reserva].setcostoPorKm(costoPorKm);
+				
+				//Agregar asignacion de chofer y placa
+				
+				costoTotal=(distanciaKm*costoPorKm)+(cantidadPasajeros*costoPorPasajero);
+				reservas[reserva].setcostoTotal(costoTotal);
+				
+				reserva++; 
+				}else{
+					cout<<"---Reservas agotadas---"<<endl;
+				}
+				
+			break;
+		}
 			case 2: {
-				break;
+			
+			for(int i=0;i<reserva;i++){
+				cout<<reservas[i].toString()<<endl;
 			}
+			
+			break;
+		}
 			case 3: {
-				break;
-			}
+			
+			
+			break;
+		}
 			case 4: {
-				break;
-			}
+			
+			break;
+		}
 			case 5: {
-				break;
-			}
+			
+			
+			break;
+		}
 			case 6: {
-				cout << endl << "--- Muchas gracias por su atencion. Feliz viaje. ---";
-				break;
-			}
+			cout << endl << "--- Muchas gracias por su atencion. Feliz viaje. ---";
+			break;
+		}
 			default: {
-				cout << "---ERROR--" << endl; 
-				cout << "Opcion incorrecta" << endl;
-			}
+			cout << "---ERROR--" << endl; 
+			cout << "Opcion incorrecta" << endl;
+		}
 		}
 	} while(op!=6);
 	
